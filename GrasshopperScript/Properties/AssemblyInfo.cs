4d59b5b90acc151d3c95c4f233a9976ea275b404
paolo cappelletto <flecheria@yahoo.it>
2017-09-22 09:56:15 +0200

    added all project files

---
+++ GrasshopperScript/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GrasshopperScript")]
+[assembly: AssemblyDescription("Static library for writing Grasshopper script inside VisualStudio")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Paolo Cappelletto")]
+[assembly: AssemblyProduct("GrasshopperScript")]
+[assembly: AssemblyCopyright("Copyright © Paolo Cappelletto 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("733d7d98-2838-4b8d-9853-2a5f5ca90955")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
+
